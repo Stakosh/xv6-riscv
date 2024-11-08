776c877ab41792b51e8958e61817c6fdc598490a
Stakosh <soto.javi1@gmail.com>
2024-11-08 15:48:53 -0300

    fix-1

--- kernel/proc.h
+++ kernel/proc.h
@@ -1,4 +1,4 @@
-// Saved registers for kernel context switches.
+
 struct context {
   uint64 ra;
   uint64 sp;
@@ -91,6 +91,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
